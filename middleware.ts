e0dfb184fb4f3ef4ec8604e343a7031fdc518e5b
Wildan Muhammad Sofwan <wildansofwan@gmail.com>
2024-04-11 00:51:47 +0700

    change middleware logic

--- middleware.ts
+++ middleware.ts
@@ -8,29 +8,54 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const response = new NextResponse();
+
   const token = request.cookies.get("token")?.value || "";
-  // console.log(token);
-  try {
-    const { payload } = await jwtVerify(
-      token as string,
-      new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_TOKEN)
-    );
-    // console.log(payload);
-    if (payload) {
-      const now = new Date();
-      const exp = new Date(Number((payload?.exp as number) * 1000));
-      // console.log(now);
-      // console.log(exp);
-      if (now >= exp) {
-        request.cookies.delete("token");
-        return NextResponse.redirect(new URL("/signin", request.url));
+
+  if (token) {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/profile`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // const data = res.status;
+
+      if (res.status == 200) {
+        return NextResponse.next();
       }
-      return NextResponse.next();
+      // console.log(data);
+      // if (data.success) {
+
+      // }
+    } catch (error) {
+      console.log(error);
+      //  return NextResponse.redirect(new URL("/signin", request.url));
     }
-  } catch (err) {
-    console.log(err);
   }
   return NextResponse.redirect(new URL("/signin", request.url));
+  // console.log(token);
+  // try {
+  //   const { payload } = await jwtVerify(
+  //     token as string,
+  //     new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_TOKEN)
+  //   );
+  //   // console.log(payload);
+  //   if (payload) {
+  //     const now = new Date();
+  //     const exp = new Date(Number((payload?.exp as number) * 1000));
+  //     // console.log(now);
+  //     // console.log(exp);
+  //     if (now >= exp) {
+  //       request.cookies.delete("token");
+  //       return NextResponse.redirect(new URL("/signin", request.url));
+  //     }
+  //     return NextResponse.next();
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  // }
+  // return NextResponse.redirect(new URL("/signin", request.url));
   // if (!payload) {
   //   return NextResponse.redirect(new URL("/signin", request.url));
   // }
