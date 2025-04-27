6495b731f4438db8f02250645ef1cba432b44e28
WIldan Muhammad Sofwan <wildan.sofwan@dretail.id>
2025-04-27 23:06:38 +0700

    f

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,39 @@
 import { getCookie } from "cookies-next";
-import { jwtVerify } from "jose";
+import { verifyToken } from "./app/lib/auth";
 // import Cookies from "universal-cookie";
 
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { deleteCookie } from "cookies-next";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
-  response.cookies.delete('token')
+  response.cookies.delete("token");
 
   const token = request.cookies.get("token")?.value || "";
 
   if (token) {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/profile`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const decoded = await verifyToken(token);
+      console.log("decode", decoded);
+      //   const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/profile`, {
+      //     method: "GET",
+      //     headers: {
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //   });
 
-      if (res.status == 200) {
+      if (decoded) {
         return NextResponse.next();
       }
     } catch (error) {
       console.log(error);
     }
   }
-  // console.log(request.cookies)
-  // deleteCookie("token");
+  console.log(request.cookies);
+  deleteCookie("token");
   return NextResponse.redirect(new URL("/signin", request.url));
 }
 
