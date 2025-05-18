caf2a58ba9f4172d002a0123ba71b40746c7cd22
WIldan Muhammad Sofwan <wildan.sofwan@dretail.id>
2025-05-18 11:52:54 +0700

    d

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import { FaChevronUp  } from "react-icons/fa";
 
 const ScrollToTopButton = () => {
-    const [visible, setVisible] = useState<boolean>(true);
+    const [visible, setVisible] = useState<boolean>(false);
     useEffect(() => {
         const toggleVisible = () =>{
             setVisible(window.scrollY > 300)
         }
-
+        // toggleVisible();
         window.addEventListener('scroll', toggleVisible);
     }, []);
     const scrollToTop = () => {
@@ -17,7 +17,7 @@ const ScrollToTopButton = () => {
     return <>
         <button
         onClick={scrollToTop}
-        className={`fixed right-8 bottom-6 text-3xl z-99 bg-azure-400 w-12 h-12 rounded-full text-white flex items-center justify-center duration-300 ${visible ? 'translate-y-0' : 'translate-y-20'}`}>
+        className={`fixed right-8 bottom-6 text-3xl z-99 bg-azure-400 w-12 h-12 rounded-full text-white flex items-center justify-center duration-300 translate-y-20 ${visible && '!translate-y-0'}`}>
             <FaChevronUp  />
         </button>
     </>;
