c15f31ba638ce7a25478b7928a08860715186cd4
WIldan Muhammad Sofwan <wildan.sofwan@dretail.id>
2025-06-09 17:01:11 +0700

    city

--- components/Search/Default/HeaderCity.tsx
+++ components/Search/Default/HeaderCity.tsx
@@ -1,4 +1,5 @@
 
+import Link from 'next/link';
 import { useCity } from './CityContext';
 
 
@@ -6,7 +7,7 @@ const HeaderCity = () => {
     const { city } = useCity();
     return  <>
         {
-            city && <h2 className="text-md font-semibold opacity-80">{city.name}</h2>
+            city && <Link href={`/search?q=${city.name}`} className="text-md font-semibold opacity-80">{city.name}</Link>
         }
     </>
 }
